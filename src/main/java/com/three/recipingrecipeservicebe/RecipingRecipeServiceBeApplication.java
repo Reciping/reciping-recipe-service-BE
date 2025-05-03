47786ae0a28bdf63ccc604b39d54237d8ada5d7b
beginninggrace <tldms123455@naver.com>
2025-05-03 14:06:50 +0900

    :tada: first commit

---
+++ src/main/java/com/three/recipingrecipeservicebe/RecipingRecipeServiceBeApplication.java
@@ -0,0 +1,13 @@
+package com.three.recipingrecipeservicebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipingRecipeServiceBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipingRecipeServiceBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/three/recipingrecipeservicebe/RecipingRecipeServiceBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.three.recipingrecipeservicebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipingRecipeServiceBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
