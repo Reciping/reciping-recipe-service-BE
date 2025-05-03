eff762183c8168db3af4098369a6b2aba3ce5a43
beginninggrace <tldms123455@naver.com>
2025-05-03 14:33:10 +0900

    :wrench: chore : config 초기세팅

---
+++ src/main/java/com/three/recipingrecipeservicebe/common/timestamp/Timestamp.java
@@ -0,0 +1,33 @@
+package com.three.recipingrecipeservicebe.common.timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Timestamp {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column
+    private LocalDateTime deletedAt;
+
+    @PrePersist
+    public void prePersist() {
+        ZoneId zoneId = ZoneId.of("Asia/Seoul");
+        ZonedDateTime now = ZonedDateTime.now(zoneId);
+        this.createdAt = now.toLocalDateTime();
+    }
+
+}
