a8af28381bb474f75bf6a2e10cae30f71deca668
Sanha Kang(Paradoxlover9417) <tksgk853@gmail.com>
2025-05-07 19:18:20 +0900

    :sparkles: feat : 레시피 서비스 및 컨트롤러 구현

---
+++ src/main/java/com/three/recipingrecipeservicebe/recipe/entity/RecipeMapper.java
@@ -0,0 +1,23 @@
+package com.three.recipingrecipeservicebe.recipe.entity;
+
+import com.three.recipingrecipeservicebe.recipe.dto.RecipeCreatedResponseDto;
+import com.three.recipingrecipeservicebe.recipe.dto.RecipeListResponseDto;
+import com.three.recipingrecipeservicebe.recipe.dto.RecipeRequestDto;
+import com.three.recipingrecipeservicebe.recipe.dto.RecipeDetailResponseDto;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        builder = @Builder(disableBuilder = true),
+        unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface RecipeMapper {
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateFromDto(RecipeRequestDto dto, @MappingTarget Recipe entity);
+
+    RecipeDetailResponseDto toDto(Recipe recipe);
+
+    RecipeListResponseDto toListDto(Recipe recipe);
+
+    RecipeCreatedResponseDto toCreatedDto(Recipe recipe);
+}
--- src/main/java/com/three/recipingrecipeservicebe/recipe/service/RecipeService.java
+++ src/main/java/com/three/recipingrecipeservicebe/recipe/service/RecipeService.java
@@ -1,14 +1,123 @@
 package com.three.recipingrecipeservicebe.recipe.service;
 
+import com.three.recipingrecipeservicebe.global.exception.custom.FileUploadException;
+import com.three.recipingrecipeservicebe.global.exception.custom.ForbiddenException;
+import com.three.recipingrecipeservicebe.recipe.dto.RecipeCreatedResponseDto;
+import com.three.recipingrecipeservicebe.recipe.dto.RecipeListResponseDto;
+import com.three.recipingrecipeservicebe.recipe.dto.RecipeRequestDto;
+import com.three.recipingrecipeservicebe.recipe.dto.RecipeDetailResponseDto;
+import com.three.recipingrecipeservicebe.recipe.entity.Recipe;
+import com.three.recipingrecipeservicebe.recipe.entity.RecipeMapper;
+import com.three.recipingrecipeservicebe.recipe.infrastructure.s3.S3Uploader;
+import com.three.recipingrecipeservicebe.recipe.mapper.RecipeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @Slf4j(topic = "RecipeService")
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 @Service
 public class RecipeService {
+
+    private final RecipeRepository recipeRepository;
+    private final RecipeMapper recipeMapper;
+    private final S3Uploader s3Uploader;
+
+    @Transactional(readOnly = true)
+    public RecipeDetailResponseDto getRecipeById(Long id) {
+        Recipe recipe = recipeRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("레시피를 찾을 수 없습니다."));
+        return recipeMapper.toDto(recipe);
+    }
+
+    @Transactional(readOnly = true)
+    public List<RecipeListResponseDto> getRecipeListByPage(Pageable pageable) {
+        return recipeRepository.findPagedByCreatedAtDesc(pageable).stream()
+                .map(recipeMapper::toListDto)
+                .toList();
+    }
+
+    @Transactional
+    public RecipeCreatedResponseDto createRecipe(RecipeRequestDto dto, Long userId, MultipartFile file) throws FileUploadException {
+        String imageUrl = processImageIfExists(file);
+
+        dto = dto.toBuilder()
+                .imageUrl(imageUrl)
+                .build();
+
+        Recipe recipe = Recipe.createFromDto(dto, userId);
+
+        Recipe savedRecipe = recipeRepository.save(recipe);
+        return recipeMapper.toCreatedDto(savedRecipe);
+    }
+
+    @Transactional
+    public void updateRecipe(Long id, RecipeRequestDto dto, Long userId, MultipartFile file) {
+        Recipe recipe = recipeRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("레시피를 찾을 수 없습니다."));
+
+        if (!recipe.getUserId().equals(userId)) {
+            throw new ForbiddenException("수정 권한이 없습니다.");
+        }
+
+        dto = processImageUpdate(recipe, dto, file);
+
+        recipe.updateFromDto(dto);
+    }
+
+    @Transactional
+    public void deleteRecipe(Long id, Long userId) {
+        Recipe recipe = recipeRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("레시피를 찾을 수 없습니다."));
+
+        if (!recipe.getUserId().equals(userId)) {
+            throw new ForbiddenException("수정 권한이 없습니다.");
+        }
+
+        recipeRepository.delete(recipe);
+    }
+
+    private String processImageIfExists(MultipartFile file) {
+        if (file == null || file.isEmpty()) return null;
+
+        try {
+            return s3Uploader.upload(file);
+        } catch (IOException e) {
+            throw new FileUploadException("파일 업로드 실패");
+        }
+    }
+
+    private RecipeRequestDto processImageUpdate(Recipe recipe, RecipeRequestDto dto, MultipartFile file) {
+        String imageUrl = dto.getImageUrl();
+
+        // 이미지 삭제 요청
+        if (Boolean.TRUE.equals(dto.getShouldRemoveImage())) {
+            if (imageUrl != null) {
+                s3Uploader.delete(imageUrl); // imageUrl 직접 삭제
+            }
+            imageUrl = null;
+        }
+
+        // 이미지 업로드 요청
+        if (file != null && !file.isEmpty()) {
+            if (imageUrl != null) {
+                s3Uploader.delete(imageUrl); // 기존 이미지 삭제
+            }
+            imageUrl = processImageIfExists(file); // 새 이미지 업로드 후 URL 반환
+        }
+
+        return dto.toBuilder()
+                .imageUrl(imageUrl)
+                .build();
+    }
+
+    // TODO [강산하] [2025-05-05]: 엘라스틱서치 일치율 검색 결과 ID 리스트로 전달받아, 해당 ID 기준으로 쿼리 호출하도록 구현
+    // TODO [강산하] [2025-05-05] : recipeId 기준으로 MongoDB 로부터 댓글 리스트 조회 후 반환
 
 }
