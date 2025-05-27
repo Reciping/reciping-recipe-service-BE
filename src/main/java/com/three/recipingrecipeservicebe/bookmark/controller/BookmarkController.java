a2ce1c48e00519c4198a00204712e5f4e72a8d82
Sanha Kang(Paradoxlover9417) <tksgk853@gmail.com>
2025-05-27 18:14:59 +0900

    :sparkles: feat : 컨트롤러에 로그 추가

--- src/main/java/com/three/recipingrecipeservicebe/bookmark/controller/BookmarkController.java
+++ src/main/java/com/three/recipingrecipeservicebe/bookmark/controller/BookmarkController.java
@@ -3,7 +3,12 @@ package com.three.recipingrecipeservicebe.bookmark.controller;
 import com.three.recipingrecipeservicebe.bookmark.dto.BookmarkRequestDto;
 import com.three.recipingrecipeservicebe.bookmark.dto.BookmarkResponseDto;
 import com.three.recipingrecipeservicebe.bookmark.service.RecipeBookmarkService;
+import com.three.recipingrecipeservicebe.global.logger.CustomLogger;
+import com.three.recipingrecipeservicebe.global.logger.LogType;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 public class BookmarkController {
 
     private final RecipeBookmarkService recipeBookmarkService;
+    private static final Logger logger = LoggerFactory.getLogger(BookmarkController.class);
 
     @GetMapping("/user/{userId}")
     public ResponseEntity<Page<BookmarkResponseDto>> getBookmarksByUser(
@@ -27,9 +33,23 @@ public class BookmarkController {
 
     @PostMapping("/toggle")
     public ResponseEntity<Boolean> toggleBookmark(
-            @RequestBody BookmarkRequestDto bookmarkRequestDto
+            @RequestBody BookmarkRequestDto bookmarkRequestDto,
+            HttpServletRequest request
     ) {
         boolean isBookmarked = recipeBookmarkService.toggleBookmark(bookmarkRequestDto);
+
+        CustomLogger.track(
+                logger,
+                LogType.VIEW,
+                "/api/v1/bookmarks/" + bookmarkRequestDto.getRecipeId(),
+                "GET",
+                String.valueOf(bookmarkRequestDto.getUserId()),
+                null,
+                null,
+                String.valueOf(isBookmarked),
+                request
+        );
+
         return ResponseEntity.ok(isBookmarked);
     }
 }
