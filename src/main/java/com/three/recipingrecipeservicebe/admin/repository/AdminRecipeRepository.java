011d8ef8aaabdd57f8f9816dec72a6a2aa3b6c81
Sanha Kang(Paradoxlover9417) <tksgk853@gmail.com>
2025-05-27 18:11:13 +0900

    :fire: remove : 관리자 기능 분리
    - 게이트웨이에 cors 설정 넣고 현 서비스에서는 제거

--- src/main/java/com/three/recipingrecipeservicebe/admin/repository/AdminRecipeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.three.recipingrecipeservicebe.admin.repository;
-
-import com.three.recipingrecipeservicebe.admin.entity.AdminRecipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRecipeRepository extends JpaRepository<AdminRecipe, Long> {
-}
