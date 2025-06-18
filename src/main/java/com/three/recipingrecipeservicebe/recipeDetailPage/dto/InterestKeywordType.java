be376062a5913f4238f3c7b739051feb74570664
Sanha Kang(Paradoxlover9417) <tksgk853@gmail.com>
2025-06-18 09:35:27 +0900

    :sparkles: feat : 이넘 추가

---
+++ src/main/java/com/three/recipingrecipeservicebe/recipeDetailPage/dto/InterestKeywordType.java
@@ -0,0 +1,11 @@
+package com.three.recipingrecipeservicebe.recipeDetailPage.dto;
+
+public enum InterestKeywordType {
+    SOLO_COOKING, // 자취요리
+    FINE_DINING, // 파인다이닝(고급요리)
+    DIET, // 다이어트
+    HEALTHY, // 건강식
+    VEGAN, // 채식
+    KIDS, // 아이요리
+    GENERAL
+}
---
+++ src/main/java/com/three/recipingrecipeservicebe/recipeDetailPage/dto/SexType.java
@@ -0,0 +1,6 @@
+package com.three.recipingrecipeservicebe.recipeDetailPage.dto;
+
+public enum SexType {
+    MALE,
+    FEMALE
+}
